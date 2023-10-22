6df691196beb5d5c2ad99f2e9f1b8844dea6f9a6
LuccianoSileo <114767558+LuccianoSileo@users.noreply.github.com>
2023-10-22 21:59:31 +0100

    Update sum4_v2.v

--- Practica1/Objetivo3/Ob-3.3/sum4_v2.v
+++ Practica1/Objetivo3/Ob-3.3/sum4_v2.v
@@ -1,6 +1,6 @@
 module sum4_v2(output wire[3:0] S, output wire c_out, input wire[3:0] A, input wire[3:0] B, input wire c_in);
 
-  wire[2:0] c;
+  wire[3:0] c;
 
   assign {c[0], S[0]} = A[0] + B[0] + c_in;
   assign {c[1], S[1]} = A[1] + B[1] + c[0];
